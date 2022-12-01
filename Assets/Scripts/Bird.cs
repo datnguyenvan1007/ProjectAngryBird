6922fab527620cad93160dedeb41572bcbbb92c3
Nguyen Van Dat <dnguyenvan344@gmail.com>
2022-12-01 16:35:13 +0700

    first commit

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    private GameController controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GameObject.Find("GameController").GetComponent<GameController>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wood"))
+        {
+            controller.DisplaySoundWoodCollision();
+        }
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            controller.DisplaySoundBirdCollideEnemy();   
+        }
+        Destroy(GameObject.Find("bird(Clone)"), 2);
+        controller.DisplaySoundBirdDestroy();
+    }
+}
